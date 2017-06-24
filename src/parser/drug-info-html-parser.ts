84b091d92afddf96c6214b9e4a74e44427871a8d
avohra <avohra@engagepeople.com>
2017-06-24 03:15:00 -0400

    drug table of contents parser completed

--- src/parser/drug-info-html-parser.ts
+++ src/parser/drug-info-html-parser.ts
@@ -4,15 +4,21 @@
 
 import * as $ from 'jquery';
 import { DrugInfo } from '../models/drug-info';
+import { DrugInfoContents } from '../models/drug-info-contents';
 
 export class DrugInfoHtmlParser {
 
   private readonly SPACER = ' ';
-  private readonly DRUG_INFO_WRAPPER_CLASS = '.brands .brand';
+  private readonly DRUG_INFO_WRAPPER_CLASS = '.chapter-wrap .brands .brand';
   private readonly DRUG_BRAND_NAME = '.brandname';
   private readonly DRUG_GENERIC_NAME = '.genericname';
   private readonly DRUG_THERAPEUTIC_CLASS = '.therapeuticclass';
-  private readonly MANUFACTURER = '.manufacturers .manufacturer';
+  private readonly MANUFACTURER = '.manufacturers .manufacturer .dita-xref';
+  private readonly REVISION_DATE = '.chapter-info td p';
+
+  private readonly DRUG_CONTENT_WRAPPER_CLASS = '.dita-topic .dita-nav-subtopics-ifp li';
+  private readonly DRUG_CONTENT_NAVIGATOR_CLASS = 'a.navigator-section';
+
 
   private htmlData: any;
 
@@ -23,27 +29,51 @@ export class DrugInfoHtmlParser {
 
   parse() {
     let drugInfo = this.parseDrugInfo();
+    drugInfo.contents = this.parseDrugContent();
+
+    console.log(drugInfo);
   }
 
   parseDrugInfo(): DrugInfo {
     let drugInfo = new DrugInfo();
-
+    drugInfo.cphaDrugId = $(this.htmlData).find('div:first-child').attr('id').replace('cpha_', '');
     drugInfo.brandName = this.getElementText(this.DRUG_INFO_WRAPPER_CLASS + this.SPACER + this.DRUG_BRAND_NAME );
     drugInfo.genericName = this.getElementText(this.DRUG_INFO_WRAPPER_CLASS + this.SPACER + this.DRUG_GENERIC_NAME);
     drugInfo.therapeuticClass = this.getElementText(this.DRUG_INFO_WRAPPER_CLASS + this.SPACER + this.DRUG_THERAPEUTIC_CLASS);
     drugInfo.manufacturers = this.getElementText(this.DRUG_INFO_WRAPPER_CLASS + this.SPACER + this.MANUFACTURER);
-
-    console.log(drugInfo);
+    drugInfo.revisionDate = this.getElementText(this.REVISION_DATE);
 
     return drugInfo;
   }
 
-  parseDrugContent() {
 
+  parseDrugContent(): Array<DrugInfoContents> {
+    const arrayContents: Array<DrugInfoContents> = [];
+    const self = this;
+    $(this.htmlData).find(this.DRUG_CONTENT_WRAPPER_CLASS).each(
+      function() {
+        const drugContents = new DrugInfoContents();
+        drugContents.text = $(this).find(self.DRUG_CONTENT_NAVIGATOR_CLASS).text().trim();
+        drugContents.anchorId = $(this).find(self.DRUG_CONTENT_NAVIGATOR_CLASS).attr('href');
+        arrayContents.push(drugContents);
+      }
+    );
+
+    return arrayContents;
   }
 
   private getElementText(classString: string) {
-    return this.elementFinder(classString).text();
+    return this.elementFinder(classString).text().trim();
+  }
+
+  private getElementsDirectText(classString: string) {
+    return (this.elementFinder(classString)
+      .contents()
+      .filter(
+        function() {
+          return this.nodeType === 3;
+        }
+      )[0].nodeValue).trim();
   }
 
   private elementFinder(classString: string) {
